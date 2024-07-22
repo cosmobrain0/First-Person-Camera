5d37fec529c15571a021131113b62e0a23895960
cosmobrain0 <cosmobrain@outlook.com>
2024-07-22 14:26:59 +0100

    nearby pickable objects are highlighted

--- Assets/Scripts/CameraSelectObject.cs
+++ Assets/Scripts/CameraSelectObject.cs
@@ -6,8 +6,13 @@ public class CameraSelectObject : MonoBehaviour
 {
     public GameObject highlightedObject;
     public bool holdingObject;
-    public Material material;
+    public Shader highlightShader;
+    Material highlightMaterial;
     public float timeMultiplier;
+    Shader highlightedObjectOriginalShader;
+
+    public float maxHighlightDistance = 2;
+    public string pickableTag;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,35 @@ public class CameraSelectObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        material.SetFloat("_TimeParam", Time.time * timeMultiplier);
+        highlightMaterial?.SetFloat("_TimeParam", Time.time * timeMultiplier);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            Vector3 hitPoint = hit.point;
+            TrySetHighlightedObject(hitObject);
+        }
+    }
+
+    void TrySetHighlightedObject(GameObject toHighlight)
+    {
+        if (highlightedObject == toHighlight) return;
+        if (highlightedObject != null)
+        {
+            highlightedObject.GetComponent<Renderer>().material.shader = highlightedObjectOriginalShader;
+            highlightedObject = null;
+            highlightedObjectOriginalShader = null;
+            highlightMaterial = null;
+        }
+        if (toHighlight == null) return;
+
+        if (toHighlight.tag == pickableTag && (toHighlight.transform.position - transform.position).sqrMagnitude <= maxHighlightDistance*maxHighlightDistance)
+        {
+            highlightedObject = toHighlight;
+            highlightedObjectOriginalShader = highlightedObject.GetComponent<Renderer>().material.shader;
+            highlightedObject.GetComponent<Renderer>().material.shader = highlightShader;
+            highlightMaterial = highlightedObject.GetComponent<Renderer>().material;
+        }
     }
 }
